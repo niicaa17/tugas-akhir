63bac219f130cdc3ccacae3f10c1c414c1ee83e7
niicaa17 <nandachairunnis17@gmail.com>
2026-07-15 23:13:47 +0700

    silence sass import deprecation warning

--- vite.config.js
+++ vite.config.js
@@ -15,6 +15,7 @@ export default defineConfig({
         preprocessorOptions: {
             scss: {
                 quietDeps: true,
+                silenceDeprecations: ['import'],
             },
         },
     },
